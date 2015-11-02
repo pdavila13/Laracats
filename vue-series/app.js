03fc71eac0797abf028b84c409edbc90c35cef7f
Paolo Davila <pdavila@iesebre.com>
2015-11-02 22:08:33 +0100

    VueJS version final OK. Working

--- vue-series/app.js
+++ vue-series/app.js
@@ -2,14 +2,86 @@ new Vue({
 	el: '#tasks',
 
 	data: {
-		tasks: [
-			{ body: 'Go to the store', completed: false }
-		]
+		tasks: [],
+
+		newTask: ''
+	},
+
+	computed: {
+		completions: function(){
+			return this.tasks.filter(function(task){
+				return task.completed;
+			});
+		},
+
+		remaining: function(){
+			return this.tasks.filter(function(task){
+				return task.completed;
+			});
+		}
+	},
+
+	filters: {
+		inProcess: function(tasks){
+			return tasks.filter(function(task) {
+				return ! task.completed;
+			});
+		}
 	},
 
 	methods: {
-		addTask: function() {
-			alert('adding a task');
+		addTask: function(e){
+
+			e.preventDefault();
+
+			if ( ! this.newTask) return;
+
+			this.tasks.push({
+				body: this.newTask,
+
+				completed: false
+			});
+
+			this.newTask = '';
+		},
+
+		/*
+		completeTask: function(task){
+			task.completed = true;
+		},
+		*/
+
+		toggleTaskCompletion: function(task){
+			task.completed = ! task.completed;
+		},
+
+		completeAll: function(task){
+			this.tasks.forEach(function(task){
+				task.completed = true;
+			});
+		},
+
+		editTask: function(task){
+			//remove the task
+			this.removeTask(task);
+
+			//update the newTask input
+			this.newTask = task.body;
+
+			//focus the newTask input
+			this.$$.newTask.focus();
+		},
+
+		removeTask: function(task){
+			
+			//remove task
+			this.tasks.$remove(task);
+		},
+
+		clearCompleted: function(){
+			this.tasks = this.tasks.filter(function(task){
+				return ! task.completed;
+			});
 		}
 	}
 
